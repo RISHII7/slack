a705a1172b69930a98989d6d527f501fad21e70d
RISHII7 <rishikesh.palande07@gmail.com>
2024-08-28 12:36:11 +0530

    #2 Authentication Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <Button variant="destructive">
-      Click Me!
-    </Button>
+    <AuthScreen />
   );
 }
