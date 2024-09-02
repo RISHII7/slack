17e58a2f5f271f221006860bdb4b3b4ec2f43771
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-02 12:14:03 +0530

    #7 User Information

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -21,6 +21,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
 
     const { signIn } = useAuthActions();
 
+    const [name, setName] = useState("");
     const [error, setError] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -36,7 +37,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
         };
 
         setPending(true);
-        signIn("password", { email, password, flow: "signUp" })
+        signIn("password", { name, email, password, flow: "signUp" })
             .catch(() => {
                 setError("Error signing up");
             })
@@ -71,6 +72,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             )}
             <CardContent className="space-y-5 px-0 pb-0">
                 <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+                    <Input 
+                        disabled={pending}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Full name"
+                        required
+                    />
+
                     <Input 
                         disabled={pending}
                         value={email}
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { Loader, LogOut } from "lucide-react";
+
+import { useCurrentUser } from "../hooks/use-current-user";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+export const UserButton = () => {
+
+    const { signOut } = useAuthActions();
+    const { data, isLoading } = useCurrentUser();
+
+    if (isLoading) {
+        return <Loader className="size-4 animate-spin text-muted-foreground" />
+    };
+
+    if (!data) {
+        return null;
+    };
+
+    const { name, image } = data;
+
+    const avatarFallback = name!.charAt(0).toUpperCase();
+
+    return (
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger className="outline-none relative" asChild>
+                <Avatar className="size-10 hover:opacity-75 transition"> 
+                    <AvatarImage alt={name} src={image} />
+                    <AvatarFallback className="bg-purple-500 text-black">
+                        {avatarFallback}
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center" side="right" className="w-60">
+                <DropdownMenuItem onClick={() => signOut()} className="h-10">
+                    <LogOut className="size-4 mr-2" />
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
