55b8eeb1ecca3f48fa491c578d3b5f86c7bf5b47
RISHII7 <rishikesh.palande07@gmail.com>
2024-10-27 22:58:18 +0530

    #16 Invite Modal

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -4,6 +4,7 @@ import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
 import { Doc } from "../../../../convex/_generated/dataModel";
 
 import { PreferencesModal } from "./preferences.modal";
+import { InviteModal } from "./invite-modal";
 
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
@@ -16,9 +17,11 @@ interface WorkspaceHeaderProps {
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
     const [preferencesOpen, setPreferencesOpen] = useState(false);
+    const [inviteOpen, setInviteOpen] = useState(false);
 
     return (
         <>
+            <InviteModal open={inviteOpen} setOpen={setInviteOpen} name={workspace.name} joinCode={workspace.joinCode} />
             <PreferencesModal open={preferencesOpen} setOpen={setPreferencesOpen} initialValue={workspace.name} />
             <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
                 <DropdownMenu>
@@ -47,7 +50,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
                         {isAdmin && (
                             <>
                                 <DropdownMenuSeparator />
-                                <DropdownMenuItem className="cursor-pointer py-2" onClick={() => { }}>
+                                <DropdownMenuItem className="cursor-pointer py-2" onClick={() => setInviteOpen(true)}>
                                     Invite people to {workspace.name}
                                 </DropdownMenuItem>
                                 <DropdownMenuSeparator />
