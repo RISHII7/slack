0a9b744231247d99d421354a9e7ef2cac7216c7d
RISHII7 <rishikesh.palande07@gmail.com>
2024-10-27 18:43:12 +0530

    #14 Workspace channels and members section

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,18 +1,26 @@
-import { AlertTriangle, Loader } from "lucide-react";
+import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
 
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
+import { SidebarItem } from "./sidebar-item";
 import { WorkspaceHeader } from "./workspace-header";
+import { WorkspaceSection } from "./workspace-section";
+import { UserItem } from "./user-item";
 
 export const WorkspaceSidebar = () => {
     const workspaceId = useWorkspaceId();
 
     const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
-    const { data:workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
-    
+    const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+    const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
+    const { data: members, isLoading: membersLoading } = useGetMembers({ workspaceId });
+
+
 
     if (workspaceLoading || memberLoading) {
         return (
@@ -35,7 +43,47 @@ export const WorkspaceSidebar = () => {
 
     return (
         <div className="flex flex-col bg-[#5e2c5f] h-full">
-            <WorkspaceHeader workspace={workspace} isAdmin = {member.role === "admin"} />
+            <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
+            <div className="flex flex-col px-2 mt-3">
+                <SidebarItem
+                    label="Threads"
+                    icon={MessageSquareText}
+                    id="threads"
+                />
+                <SidebarItem
+                    label="Drafts & Sent"
+                    icon={SendHorizonal}
+                    id="drafts"
+                />
+            </div>
+            <WorkspaceSection
+                label="Channels"
+                hint="New channel"
+                onNew={() => { }}
+            >
+                {channels?.map((item) => (
+                    <SidebarItem
+                        key={item._id}
+                        icon={HashIcon}
+                        label={item.name}
+                        id={item._id}
+                    />
+                ))}
+            </WorkspaceSection>
+            <WorkspaceSection
+                label="Direct messages"
+                hint="New direct message"
+                onNew={() => { }}
+            >
+                {members?.map((item) => (
+                    <UserItem
+                        key={item._id}
+                        id={item._id}
+                        label={item.user.name}
+                        image={item.user.image}
+                    />
+                ))}
+            </WorkspaceSection>
         </div>
     );
 };
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -28,9 +28,9 @@ export const UserButton = () => {
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative" asChild>
-                <Avatar className="size-10 hover:opacity-75 transition"> 
-                    <AvatarImage alt={name} src={image} />
-                    <AvatarFallback className="bg-purple-500 text-black">
+                <Avatar className="rounded-md size-10 hover:opacity-75 transition"> 
+                    <AvatarImage className="rounded-md" alt={name} src={image} />
+                    <AvatarFallback className="rounded-md bg-sky-500 text-white">
                         {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
