c1d4427c196ce8b2424435b1b438bb4251508fc2
RISHII7 <rishikesh.palande07@gmail.com>
2024-10-28 12:11:04 +0530

    #17 Join Workspace Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Slack 2.0",
   description: "Created by Rushikesh Palande",
+  icons: {
+    icon: "/logo.svg"
+  }
 };
 
 export default function RootLayout({
