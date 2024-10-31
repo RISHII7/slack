73c369b5a58c90231ef09c265e165a4163b41a3e
RISHII7 <rishikesh.palande07@gmail.com>
2024-11-01 00:46:26 +0530

    #38.1 Debugging workspaces and channels Delete API

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -6,7 +6,7 @@ import { mutation, query } from "./_generated/server";
 const generateCode = () => {
     const code = Array.from(
         { length: 6 },
-        () => 
+        () =>
             "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
     ).join("");
 
@@ -17,7 +17,7 @@ export const join = mutation({
     args: {
         joinCode: v.string(),
         workspaceId: v.id("workspaces"),
-    }, 
+    },
     handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
 
@@ -38,8 +38,8 @@ export const join = mutation({
         const existingMember = await ctx.db
             .query("members")
             .withIndex("by_workspace_id_user_id", (q) =>
-            q.eq("workspaceId", args.workspaceId)
-            .eq("userId", userId)
+                q.eq("workspaceId", args.workspaceId)
+                    .eq("userId", userId)
             )
             .unique();
 
@@ -70,8 +70,8 @@ export const newJoinCode = mutation({
 
         const member = await ctx.db
             .query("members")
-            .withIndex("by_workspace_id_user_id", (q) => 
-            q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+            .withIndex("by_workspace_id_user_id", (q) =>
+                q.eq("workspaceId", args.workspaceId).eq("userId", userId)
             )
             .unique();
 
@@ -126,7 +126,7 @@ export const create = mutation({
 
 export const get = query({
     args: {},
-    handler: async(ctx) => {
+    handler: async (ctx) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -156,7 +156,7 @@ export const get = query({
 
 export const getInfoById = query({
     args: { id: v.id("workspaces") },
-    handler: async(ctx, args) => {
+    handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -179,7 +179,7 @@ export const getInfoById = query({
 
 export const getById = query({
     args: { id: v.id("workspaces") },
-    handler: async(ctx, args) => {
+    handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -204,7 +204,7 @@ export const update = mutation({
         id: v.id("workspaces"),
         name: v.string(),
     },
-    handler: async(ctx, args) => {
+    handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -215,7 +215,7 @@ export const update = mutation({
             .query("members")
             .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id).eq("userId", userId))
             .unique();
-        
+
         if (!member || member.role !== "admin") {
             throw new Error("Unauthorized");
         };
@@ -226,15 +226,14 @@ export const update = mutation({
 
         return args.id;
     },
-    
-});
 
+});
 
 export const remove = mutation({
     args: {
         id: v.id("workspaces"),
     },
-    handler: async(ctx, args) => {
+    handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -245,25 +244,57 @@ export const remove = mutation({
             .query("members")
             .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id).eq("userId", userId))
             .unique();
-        
+
         if (!member || member.role !== "admin") {
             throw new Error("Unauthorized");
         };
 
-        const [members] = await Promise.all([
+        const [members, channels, conversations, messages, reactions] = await Promise.all([
             ctx.db
                 .query("members")
                 .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
-                .collect()
+                .collect(),
+            ctx.db
+                .query("channels")
+                .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+                .collect(),
+            ctx.db
+                .query("conversations")
+                .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+                .collect(),
+            ctx.db
+                .query("messages")
+                .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+                .collect(),
+            ctx.db
+                .query("reactions")
+                .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+                .collect(),
+
         ]);
 
         for (const member of members) {
             await ctx.db.delete(member._id)
+        };
+
+        for (const channel of channels) {
+            await ctx.db.delete(channel._id)
         }
 
+        for (const conversation of conversations) {
+            await ctx.db.delete(conversation._id)
+        }
+
+        for (const message of messages) {
+            await ctx.db.delete(message._id)
+        }
+
+        for (const reaction of reactions) {
+            await ctx.db.delete(reaction._id)
+        }
+        
         await ctx.db.delete(args.id);
 
         return args.id;
     },
-    
 });
